ea22a129358c5e4904d30b25303f1673622d1084
saikumar8877 <49802997+saikumar8877@users.noreply.github.com>
2019-11-19 22:57:50 +0530

    ok

---
+++ project11/App11/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+from project11 import settings as sai
+from django.shortcuts import HttpResponse
+
+def registration(request):
+    return render(request,"one.html")
+
+def elogin(request):
+    username=request.POST.get("uname")
+    password=request.POST.get("pwd")
+
+    if username=="Sathya" and password=="Tech":
+         subject = "Test Mail"
+         message = " Congratulations"
+         send_mail(subject, message, sai.EMAIL_HOST_USER, ["saikumar8877@gmail.com"])
+         return render(request,"welcome.html")
+    else:
+
+        return render(request,"one.html" ,{"newmessage":"Invalid user details"})
+def logout(request):
+    return render(request,"one.html")
+
+
+
+
